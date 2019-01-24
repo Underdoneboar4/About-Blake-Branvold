a5b7ffffdc2e2871351669b58704505fcb8a940a
90304593 <90304593@hs-03319-macair.ad.edenpr.org>
2019-01-24 12:29:00 -0600

    This is About Blake Branvold

--- About Blake Branvold/ViewController.swift
+++ About Blake Branvold/ViewController.swift
@@ -19,6 +19,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var LabelText: UILabel!
+  
+    @IBOutlet weak var ImageChange: UIImageView!
+    
+    @IBOutlet weak var SpaghetButton: UIButton!
+    
+    @IBOutlet weak var ThumbUpButton: UIButton!
+    
+    @IBOutlet weak var OkaySymbolButton: UIButton!
+    
+    @IBOutlet weak var FlippedButton: UIButton!
+    
+    @IBAction func ChangeToImg1(_ sender: Any) {
+        ImageChange.image = #imageLiteral(resourceName: "BlakePostSpaghetti")
+        LabelText.text = "Spaghetti time!"
+    }
+    @IBAction func ChangeToImg2(_ sender: Any) {
+        ImageChange.image = #imageLiteral(resourceName: "BlakeThumbUp")
+        LabelText.text = "Thumbs up broski!"
+    }
+    @IBAction func ChangeToImg3(_ sender: Any) {
+        ImageChange.image = #imageLiteral(resourceName: "BlakeOkeyFrog")
+        LabelText.text = "Idk how I feel about this one"
+    }
+    @IBAction func ChangeToImg4(_ sender: Any) {
+        ImageChange.image = #imageLiteral(resourceName: "BlakePreSpaghetti")
+        LabelText.text = "Flipped sideways! No idea why"
+    }
 
 
 }
